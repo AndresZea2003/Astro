cde770c5fa0466c5883d3dcd239cb5f9bfa8ec4f
Andres Felipe Zea Acevedo <andres.zea@evertecinc.com>
2024-06-03 10:36:02 -0500

    feat: chatbot added

---
+++ src/components/ChatBot.vue
@@ -0,0 +1,170 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import ProfileUser from "./ProfileUser.vue";
+
+const message = ref("");
+const rows = ref(1);
+const maxRows = 9;
+const token = import.meta.env.PUBLIC_OPENAI_API_KEY;
+
+const updateRows = () => {
+  const lineBreaks = message.value.split("\n").length;
+  rows.value = lineBreaks > maxRows ? maxRows : lineBreaks;
+};
+
+const allowNewLine = (event) => {
+  event.stopPropagation();
+};
+
+const handleKeyDown = (event) => {
+  if (event.key === "Enter" && !event.shiftKey) {
+    event.preventDefault();
+    sendMessage();
+  }
+};
+
+let messages = ref([
+  {
+    isUser: false,
+    message: "Hola! Soy un mensaje de prueba",
+  },
+]);
+
+const sendMessage = () => {
+  let input = document.getElementById("message");
+  messages.value.push({
+    isUser: true,
+    message: input.value,
+  });
+
+  axios
+    .post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "Tu eres un asistente llamado PavoGranaino",
+          },
+          {
+            role: "user",
+            content: input.value,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response.data.choices[0].message.content);
+      messages.value.push({
+        isUser: false,
+        message: response.data.choices[0].message.content,
+      });
+    })
+    .catch((error) => {
+      res.value = error;
+    });
+
+  input.value = "";
+  rows.value = 1;
+  message.value = "";
+};
+</script>
+<template>
+  <div class="container mx-auto p-4 min-h-screen flex flex-col">
+    <div
+      class="bg-[#C73659] border-4 border-[#A91D3A] rounded p-4 mx-auto w-full"
+    >
+      <h1 class="text-3xl font-semibold text-red-900">ChatBot</h1>
+    </div>
+
+    <div class="mt-6 grid grid-cols-4 gap-6 flex-grow">
+      <div
+        style="height: 84vh"
+        class="bg-[#EEEEEE] border-2 border-[#C73659] p-6 rounded-md h-full"
+      >
+        sidebar
+      </div>
+
+      <div
+        id="chat"
+        style="height: 84vh"
+        class="bg-[#EEEEEE] border-2 border-[#C73659] p-6 rounded-md col-span-3 flex flex-col overflow-y-auto justify-between"
+      >
+        <div class="overflow-y-auto flex-grow">
+          <div v-for="msg in messages">
+            <ProfileUser
+              :isUser="msg.isUser"
+              :message="msg.message"
+              style="white-space: pre-wrap"
+            />
+          </div>
+        </div>
+
+        <div
+          :class="`flex bg-gray-300 rounded-md w-full p-2 mt-6
+          ${rows === 1 ? 'items-center' : 'items-end'}`"
+        >
+          <button
+            class="flex items-center justify-center w-10 h-10 text-gray-400 hover:text-gray-200"
+          >
+            <!-- Icono de adjuntar (puedes usar cualquier icono que prefieras) -->
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15.172 7l-6.414 6.414a2 2 0 002.828 2.828L17 10.828a4 4 0 10-5.656-5.656l-4.95 4.95a6 6 0 108.485 8.485l1.414-1.414"
+              ></path>
+            </svg>
+          </button>
+          <textarea
+            v-model="message"
+            @input="updateRows"
+            @keydown="handleKeyDown"
+            :rows="rows"
+            style="resize: none"
+            id="message"
+            placeholder="Envía un mensaje a ChatBOT"
+            class="bg-transparent flex-1 text-gray-800 placeholder-gray-400 focus:outline-none ml-2 w-full"
+          ></textarea>
+          <button
+            @click="sendMessage()"
+            class="flex items-center justify-center w-10 h-10 bg-gray-200 hover:bg-gray-600 text-gray-400 hover:text-gray-200 rounded-full"
+          >
+            <!-- Icono de enviar -->
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 10l7-7m0 0l7 7m-7-7v18"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ProfileUser.vue
@@ -0,0 +1,64 @@
+<script setup>
+const props = defineProps({
+  isUser: {
+    type: Boolean,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="mx-4 pb-2">
+    <div
+      v-if="props.isUser"
+      :class="`w-full flex items-center ${
+        props.isUser ? 'justify-end' : 'justify-start'
+      }`"
+    >
+      <div
+        class="bg-gray-400 border-4 border-gray-500 mx-6 p-3 rounded-md shadow-lg"
+      >
+        {{ props.message }}
+      </div>
+      <div
+        style="
+          width: 60px;
+          height: 60px;
+          min-width: 60px;
+          max-width: 60px;
+          min-height: 60px;
+          max-height: 60px;
+        "
+        class="bg-blue-800 rounded-full"
+      ></div>
+    </div>
+
+    <div
+      v-else
+      :class="`w-full flex items-center ${
+        props.isUser ? 'justify-end' : 'justify-start'
+      }`"
+    >
+      <div
+        style="
+          width: 60px;
+          height: 60px;
+          min-width: 60px;
+          max-width: 60px;
+          min-height: 60px;
+          max-height: 60px;
+        "
+        class="bg-red-800 rounded-full"
+      ></div>
+      <div
+        class="bg-gray-400 border-4 border-gray-500 mx-6 p-3 rounded-md shadow-lg"
+      >
+        {{ props.message }}
+      </div>
+    </div>
+  </div>
+</template>
