66883f3f9eff95bec377c77177bcb66ff13c87d1
Andres Felipe Zea Acevedo <andres.zea@evertecinc.com>
2024-06-04 00:16:11 -0500

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   env: {
-    PUBLIC_OPENAI_API_KEY: import.meta.env.PUBLIC_OPENAI_API_KEY,
+    PUBLIC_OPENAI_API_KEY: import.meta.env.PUBLIC_OPENAI_API_KEY
   },
   output: "server",
+  adapter: vercel({
+    imageService: true,
+  }),
 });
